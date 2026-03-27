a48e0515e16474cd28dc0ec4eee36aeb01af83f3
Robin <100517796+BjorStar@users.noreply.github.com>
2026-03-27 12:35:31 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,22 @@
 from fastapi import FastAPI, Request
-from api.ip import router as ip_router
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-# This loads /api/ip from your router file
-app.include_router(ip_router, prefix="/api")
+@app.get("/")
+def read_root():
+    return { "msg": "Hello local docker"}
+
+@app.get("/api/ip")
+def ip(request: Request):
+    return { "ip": request.client.host}
 
 
+@app.get("/ip", response_class=HTMLResponse)
+def ip(request: Request):
+    return f"<h1>Din ip är {request.client.host}</h1>"
 
-
-
-
-
+@app.get("/hello")
+def hello():
+    return { "msg": "Hello there"}
 
