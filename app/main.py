2558b109a7ec9f799693dad05df6db636e28e446
Robin <100517796+BjorStar@users.noreply.github.com>
2026-03-27 12:04:30 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
+from api.ip import router as ip_router
 
 app = FastAPI()
+
+
+app.include_router(ip_router, prefix="/api")
 
 @app.get("/api/ip")
 async def read_ip(request: Request):
     return {"client_ip": request.client.host}
+
+
+
+
+
+
+
 
